f7289fec845e6fbf995ed1ad4c3ad54d38ef2a65
soyda <stevendruesne@gmail.com>
2026-04-07 13:56:38 +0200

    Narrow credential modal further

--- app/ui/item_editor_dialog.py
+++ app/ui/item_editor_dialog.py
@@ -188,7 +188,7 @@ class CredentialItemEditorDialog(QDialog):
     ) -> None:
         super().__init__(parent)
         self.setWindowTitle(title)
-        self.resize(620, 310)
+        self.resize(560, 310)
 
         self._extra_metadata: dict = {}
         self._extra_payload: dict = {}
